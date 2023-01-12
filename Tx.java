dd4cfca149cfdf3cf88548045b0323060ff1e626
sanbo <sanbo.xyz@gmail.com>
2023-01-12 13:07:23 +0800

    update sth. at 2023-01-12 13:07:23

--- Tx.java
+++ Tx.java
@@ -5,16 +5,15 @@ import java.util.Properties;
 public class Tx {
     public static void main(String[] args) {
         Properties ps = System.getProperties();
-        System.out.println("ps" + ps);
-        System.out.println("===============Properties=================");
+        System.out.println("===================Properties=====================");
         for (Map.Entry entry : ps.entrySet()) {
             System.out.println(entry.getKey() + "_______" + entry.getValue());
         }
 
         Map<String, String> maps = System.getenv();
 
-        System.out.println("===============env=================");
-        for (Map.Entry entry : maps.entrySet()) {
+        System.out.println("===================env=====================");
+        for (Map.Entry<String, String> entry : maps.entrySet()) {
             System.out.println(entry.getKey() + "-------------------" + entry.getValue());
         }
     }
